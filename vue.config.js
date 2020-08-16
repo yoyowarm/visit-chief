29f6205c5cd440dce980c67b6e376686f1a4c622
yoyo <po8867@gmail.com>
2020-08-16 10:44:56 +0800

    Modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
      * In most cases please use '/' !!!
      * Detail: https://cli.vuejs.org/config/#publicpath
      */
-    publicPath: '../',
+    publicPath: './',
     assetsDir: 'static',
     // lintOnSave: process.env.NODE_ENV === 'development',
     lintOnSave: false,
